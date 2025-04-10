a22dbc53ee5ace082da15ddb34ed2e8b95add02c
Neunerlei <code@neunerlei.eu>
2025-04-10 11:08:48 +0200

    feat(8.4 debian): make entrypoint.sh (was boot.sh) aware of the composer commands

--- src/php/8.4/fpm-debian/bin/boot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This allows the final image to hook in it's own script files
-if [ -f ${BASH_SOURCE%/*}/boot.local.sh ]; then
-	source ${BASH_SOURCE%/*}/boot.local.sh;
-fi
-
-# Start the supervisor
-/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf

ab1009a4c40f1704c31cbd6411e8705d17509077
neunerlei <ich@neunerlei.eu>
2025-10-28 22:33:50 +0100

    fix(php-8.4-fpm-debian): use the correct /usr/bin/app directory instead of /user/bin...

--- src/php/8.4/fpm-debian/bin/entrypoint.sh
+++ src/php/8.4/fpm-debian/bin/entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 # This allows the final image to hook in it's own script files
-if [ -f ${BASH_SOURCE%/*}/entrypoint.local.sh ]; then
-	source ${BASH_SOURCE%/*}/entrypoint.local.sh;
+if [ -f /usr/bin/app/entrypoint.local.sh ]; then
+	source /usr/bin/app/entrypoint.local.sh;
+fi
+
+# For legacy reasons I still support the "/user" variants of the entrypoint, too.
+# @todo remove this in v8.5
+# DO not rely on this being present, it is only for legacy reasons!
+if [ -f /user/bin/app/entrypoint.local.sh ]; then
+  echo "WARNING: The entrypoint file '/user/bin/app/entrypoint.local.sh' is deprecated and will be removed in v8.5! Please use '/usr/bin/app/entrypoint.local.sh' instead!";
+	source /user/bin/app/entrypoint.local.sh;
 fi
 
 bash -c "${*}"

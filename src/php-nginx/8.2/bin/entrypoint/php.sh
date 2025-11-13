aa7d47c545ce6a46b709fd32c658a62887780e9b
neunerlei <ich@neunerlei.eu>
2025-11-13 03:05:35 +0100

    fix(php-nginx): do not apply prod php settings in dev

--- src/php-nginx/8.2/bin/entrypoint/php.sh
+++ src/php-nginx/8.2/bin/entrypoint/php.sh
@@ -2,7 +2,12 @@
 echo "[ENTRYPOINT.php] Configuring PHP";
 
 render_template 'PHP_TIMEZONE PHP_MEMORY_LIMIT PHP_UPLOAD_MAX_FILESIZE PHP_POST_MAX_SIZE' /etc/app/config.tpl/php/php.common.tpl.ini /usr/local/etc/php/conf.d/zzz.app.common.ini
-render_template 'PHP_PROD_DISPLAY_ERRORS PHP_PROD_DISPLAY_STARTUP_ERRORS' /etc/app/config.tpl/php/php.prod.tpl.ini /usr/local/etc/php/conf.d/zzz.app.prod.ini
 render_template 'PHP_FPM_MAX_CHILDREN PHP_FPM_START_SERVERS PHP_FPM_MIN_SPARE_SERVERS PHP_FPM_MAX_SPARE_SERVERS PHP_FPM_MAX_REQUESTS' /etc/app/config.tpl/php/fpm-pool.tpl.conf /usr/local/etc/php-fpm.d/www.conf
+
+# If the APP_ENV is set to "prod"
+if [[ "${APP_ENV}" == "prod" ]]; then
+    echo "[ENTRYPOINT.php] Detected APP_ENV=prod, applying production PHP settings";
+    render_template 'PHP_PROD_DISPLAY_ERRORS PHP_PROD_DISPLAY_STARTUP_ERRORS' /etc/app/config.tpl/php/php.prod.tpl.ini /usr/local/etc/php/conf.d/zzz.app.prod.ini
+fi
 
 echo "[ENTRYPOINT.php] PHP configuration completed";

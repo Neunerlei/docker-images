4cc1e48056da1d10bee5d3d996a2b6401e331206
neunerlei <ich@neunerlei.eu>
2026-02-05 18:28:12 +0100

    feat: export NGINX_CUSTOM_SNIPPETS_PROXY_DIR for improved configuration

--- src/nginx/1.29/container/entrypoint/step.d/015-image-vars.sh
+++ src/nginx/1.29/container/entrypoint/step.d/015-image-vars.sh
@@ -2,7 +2,7 @@
 
 echo "[ENTRYPOINT] Image configuration:";
 
-declare NGINX_CUSTOM_SNIPPETS_PROXY_DIR="${NGINX_SNIPPETS_DIR}/proxy.d"
+export NGINX_CUSTOM_SNIPPETS_PROXY_DIR="${NGINX_SNIPPETS_DIR}/proxy.d"
 
 # IMPORTANT: We ignore the "BUILD" container mode, and always set the mode based on the presence of proxy containers.
 # There is no build-time configuration for nginx images, so the build mode is not applicable here.

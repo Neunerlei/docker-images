8e7e3d086eacd6a0f1421e21f21359cc090db5a1
neunerlei <ich@neunerlei.eu>
2026-02-27 10:40:41 +0100

    fix(node-nginx): ensure clearer handling for missing NODE_WEB_COMMAND variable on boot

--- src/node-nginx/24/container/entrypoint/step.d/015-image-vars.sh
+++ src/node-nginx/24/container/entrypoint/step.d/015-image-vars.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-echo "[ENTRYPOINT] Image configuration:";
-
-# Check for default web entry script
-declare default_node_web_command=""
-declare default_web_entrypoint_script="/var/www/html/server.js"
-if [ -f "${default_web_entrypoint_script}" ]; then
-  default_node_web_command="node ${default_web_entrypoint_script}"
-fi
+echo "[ENTRYPOINT] Image configuration:"
 
 # Additional NGINX settings
 # If NGINX_TRY_FILES is empty, assign the default using strong single quotes
@@ -20,25 +13,40 @@ export NODE_SERVICE_PORT="${NODE_SERVICE_PORT:-"3000"}"
 export NODE_WORKER_PROCESS_COUNT="${NODE_WORKER_PROCESS_COUNT:-"1"}"
 export NODE_ENV="${NODE_ENV:-${ENVIRONMENT}}"
 
-if [ -z "${CONTAINER_MODE}" ] ; then
-  # If the NODE_WORKER_COMMAND is set, set the CONTAINER_MODE environment variable to "worker", otherwise set it to "web"
-  if [ -n "${NODE_WORKER_COMMAND}" ]; then
-    export CONTAINER_MODE="worker"
-  else
-    # If in web mode, we require the NODE_WEB_COMMAND to be set, otherwise exit with an error
-    if [ -z "${NODE_WEB_COMMAND}" ] && [ -e "${default_node_web_command}" ] ; then
-      echo "[ENTRYPOINT] ERROR: NODE_WEB_COMMAND is not set, but CONTAINER_MODE is 'web'. Exiting...";
-      exit 1
+if [ -z "${CONTAINER_MODE}" ]; then
+    # If the NODE_WORKER_COMMAND is set, set the CONTAINER_MODE environment variable to "worker", otherwise set it to "web"
+    if [ -n "${NODE_WORKER_COMMAND}" ]; then
+        export CONTAINER_MODE="worker"
+    else
+        export NODE_WEB_COMMAND="${NODE_WEB_COMMAND:-${default_node_web_command}}"
+        export CONTAINER_MODE="web"
+        feature_registry="${feature_registry} nginx"
     fi
+    feature_registry="${feature_registry} supervisor"
+fi
 
-    export NODE_WEB_COMMAND="${NODE_WEB_COMMAND:-${default_node_web_command}}"
-    export CONTAINER_MODE="web"
-    feature_registry="${feature_registry} nginx"
-  fi
-  feature_registry="${feature_registry} supervisor"
+if [ "${CONTAINER_MODE}" = "worker" ]; then
+    if [ -z "${NODE_WORKER_COMMAND}" ]; then
+        # Theoretically, if used correctly, this should never happen, because if the NODE_WORKER_COMMAND is not set,
+        # we set the CONTAINER_MODE to web, but we add this check just to be sure (if someone set CONTAINER_MODE to worker manually,
+        # but forgot to set the NODE_WORKER_COMMAND, we want to catch this error and exit, instead of running an empty command and
+        # exiting with a more cryptic error)
+        echo "[ENTRYPOINT] ERROR: NODE_WORKER_COMMAND is not set, but CONTAINER_MODE is 'worker'. Exiting..."
+        exit 1
+    fi
+elif [ "${CONTAINER_MODE}" = "web" ]; then
+    if [ -z "${NODE_WEB_COMMAND}" ]; then
+        if [ -e "/var/www/html/server.js" ]; then
+            export NODE_WEB_COMMAND="node /var/www/html/server.js"
+            echo "[ENTRYPOINT] INFO: NODE_WEB_COMMAND is not set, but found /var/www/html/server.js. Setting NODE_WEB_COMMAND to 'node /var/www/html/server.js'"
+        else
+            echo "[ENTRYPOINT] ERROR: NODE_WEB_COMMAND is not set, but CONTAINER_MODE is 'web'. Exiting..."
+            exit 1
+        fi
+    fi
 fi
 
 user_owned_directories_registry+=(
-  "/var/www/.npm"
-  "/usr/local/lib/node_modules"
+    "/var/www/.npm"
+    "/usr/local/lib/node_modules"
 )
